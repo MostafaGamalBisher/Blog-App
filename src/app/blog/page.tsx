f3e89ba5035b6985bbb182449542f0173744547e
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-07-26 17:00:27 +0300

    filtering phase

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,27 @@
 import PostsList from './_components/PostsList';
 import posts from './posts';
 
-const postsArray = Object.values(posts);
+interface BlogListPageProps {
+  searchParams: Promise<{ category?: string }>;
+}
 
-const BlogListPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-start">
-      <h2 className="border-b p-4 font-extrabold">The most recent Blogs</h2>
-      <PostsList posts={postsArray} />
-    </div>
-  );
-};
+export default async function BlogListPage({
+  searchParams,
+}: BlogListPageProps) {
+  const { category } = await searchParams;
 
-export default BlogListPage;
+  const postsArray = category
+    ? Object.values(posts).filter((post) => post.category === category)
+    : Object.values(posts);
+
+  if (postsArray.length > 0) {
+    return (
+      <div className="flex flex-col items-center justify-start">
+        <h2 className="border-b p-4 font-extrabold">The most recent Blogs</h2>
+        <PostsList posts={postsArray} />
+      </div>
+    );
+  } else {
+    return <p>Can not find the matched category </p>;
+  }
+}
--- src/app/blog/posts.ts
+++ src/app/blog/posts.ts
@@ -3,6 +3,7 @@ export interface Post {
   slug: string;
   date: Date;
   content: string;
+  category: string;
 }
 
 const posts: Record<string, Post> = {
@@ -11,12 +12,14 @@ const posts: Record<string, Post> = {
     slug: 'first-post',
     date: new Date('2026-01-01'),
     content: 'we have started the new era of coding',
+    category: 'typescript',
   },
   'second-post': {
     title: 'nextjs mastery',
     slug: 'second-post',
     date: new Date('2026-01-02'),
     content: 'first frame work to master',
+    category: 'nextjs',
   },
 };
 
