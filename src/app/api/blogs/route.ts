9c93cdd62ca6394352be0e825264b69bf06aa6e7
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-08-06 16:00:26 +0300

    add logging for VERCEL_PROJECT_PRODUCTION_URL in blog route

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -18,6 +18,7 @@ export function GET(request: Request) {
 
   const slicedBlogsArray = blogsArray.slice(start, end);
   console.log('VERCEL_URL:', process.env.VERCEL_URL);
+  console.log('VERCEL_URL:', process.env.VERCEL_PROJECT_PRODUCTION_URL);
   return NextResponse.json<SentRawData<Blog[]>>({
     data: slicedBlogsArray,
     meta: {
