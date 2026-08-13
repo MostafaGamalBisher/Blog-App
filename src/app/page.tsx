ffd932bf7a6339862934988a11db535189ab24db
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-08-13 16:04:01 +0300

    feat: adjust typography and add border to blog link for improved UI consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="m-5 flex flex-col items-center justify-center gap-4">
-      <p className="text-primary font-nunito text-3xl">
+      <p className="text-primary font-nunito text-xl lg:text-3xl">
         Welcome to Al-Mdrasa Blog App
       </p>
       <Link
         href={'/blog'}
-        className="bg-background font-heading animate-pulse cursor-pointer rounded-4xl p-2 text-xl transition-all"
+        className="bg-background font-heading animate-pulse cursor-pointer rounded-4xl border p-2 text-xl transition-all"
       >
         Click To Go To The Blog List
       </Link>
